ba7d0ad46e9ba448ea26264a480e669c11e16d3b
whit <whit@openplans.org>
2008-12-04 21:41:17 -0500

    add properly named ini, and test directory

---
+++ tests/test_proxy.py
@@ -0,0 +1,23 @@
+from webtest import TestApp
+from paste.deploy.loadwsgi import loadapp
+import os
+import pkg_resources
+
+dist = pkg_resources.Requirement.parse('reportedit')
+
+def setup(tc):
+    doc_path = pkg_resources.resource_filename(dist, 'docs')
+    gc = dict(data_path='reportedit:tests')
+    app2 = loadapp('config:reportedit-conf.ini',
+                   name='test',
+                   **dict(global_conf=gc, relative_to=doc_path))
+    
+    tc.app = TestApp(app2)
+
+def teardown(tc):
+    pass
+
+
+
+def test_basic(tc):
+    tc.app.get('/', status=200)
