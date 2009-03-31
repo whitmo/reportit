32f80fd8cde890225cad4ea5b0a3ac583d50501e
Ivan Willig <ivan@mao.(none)>
2009-03-31 19:13:35 -0400

    removed old version bmabr

--- py/bmabr.wsgi
+++
@@ -1,7 +0,0 @@
-import os, sys
-sys.path.append('/usr/local/reportit')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'py.settings'
-
-import django.core.handlers.wsgi
-
-application = django.core.handlers.wsgi.WSGIHandler()
--- py/trackit/admin.py
+++
@@ -1,9 +0,0 @@
-
-from webmaps.trackit.models import Issue 
-from django.contrib import admin 
-
-class IssueAdmin(admin.ModelAdmin): 
-    list_display = ('date','the_geom')
-
-admin.site.register(Issue, IssueAdmin)
-
