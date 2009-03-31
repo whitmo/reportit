dc3ca932dac95453d2c9c0ebea6b66ec70916b4e
Ivan Willig <ivan@mao.(none)>
2009-03-31 19:15:36 -0400

    add new name for project

---
+++ py/bmabr/buildmeabikerack/models.py
@@ -0,0 +1,60 @@
+from django.contrib.gis.db import models
+from django.forms import ModelForm
+
+
+# Create your models here.
+
+
+class Rack(models.Model): 
+    location = models.PointField(srid=4326)
+    date = models.DateTimeField()    
+    address = models.CharField(max_length=200)
+    meta = models.TextField()
+    contact_email = models.EmailField()
+    STATUS_STATE  = ( 
+        ('r', 'requested'),
+        ('a', 'undering assessment'),
+        ('b','built'),
+        ('n','not built')
+                      
+    )
+    status = models.CharField(max_length=1, choices=STATUS_STATE)
+    objects = models.GeoManager()
+
+    class Meta:
+        ordering = ['date']
+
+
+class Neighborhoods(models.Model):
+    gid = models.IntegerField(primary_key=True)
+    state = models.CharField(max_length=2)
+    county = models.CharField(max_length=43)
+    city = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    regionid = models.DecimalField(max_digits=65535, decimal_places=65535)
+    the_geom = models.MultiPolygonField() 
+    objects = models.GeoManager()
+
+    class Meta:
+        db_table = u'gis_neighborhoods'
+
+
+class  CommunityBoard(models.Model):
+    gid = models.IntegerField(primary_key=True)
+    borocd = models.IntegerField()
+    the_geom = models.MultiPolygonField()
+    name = models.IntegerField()
+    neighborhoods = models.CharField(max_length=100)
+    population_1980 = models.IntegerField()
+    population_1990 = models.IntegerField()
+    population_2000 = models.IntegerField()
+    borough = models.CharField(max_length=20)
+    objects = models.GeoManager()
+    class Meta:
+        db_table = u'gis_comunity_board'
+
+
+
+class RackForm(ModelForm): 
+    class Meta: 
+        model = Rack 
